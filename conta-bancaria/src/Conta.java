0e3ec03ca80728c8ecb2f13a524bcb2800101665
Lucas Martins <lucas.martins.fsa@gmail.com>
2023-07-03 12:34:36 -0300

    refatoração

--- conta-bancaria/src/Conta.java
+++ conta-bancaria/src/Conta.java
@@ -2,47 +2,50 @@ import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class Conta {
+
     private int numero;
     private String agencia;
     private String nomeDoCliente;
     private BigDecimal saldo;
     private int opcao;
 
-    public Conta(){}
+    public Conta() {
+    }
 
-    public void cadastrarConta(){
+    public void cadastrarConta() {
         Scanner sc = new Scanner(System.in);
 
-       System.out.println("-------------------------");
+        System.out.println("-------------------------");
         System.out.println("1-Adicionar Conta");
         System.out.println("0-Encerrar");
         System.out.println("-------------------------");
         opcao = sc.nextInt();
         System.out.println("--------------------------");
 
-            while (opcao == 1) {
-                System.out.println("Por favor, digite o Número da Conta! ");
-                numero = sc.nextInt();
-                System.out.println("Por favor, digite o número da Agência!");
-                agencia = sc.next();
-                System.out.println("Por favor, digite o nome do Cliente!");
-                sc.nextLine();
-                nomeDoCliente = sc.nextLine();
-                System.out.println("Saldo");
-                saldo = sc.nextBigDecimal();
-                System.out.println("---------------------------");
+        while (opcao == 1) {
+            System.out.println("Por favor, digite o Número da Conta! ");
+            numero = sc.nextInt();
+            System.out.println("Por favor, digite o número da Agência!");
+            agencia = sc.next();
+            System.out.println("Por favor, digite o nome do Cliente!");
+            sc.nextLine();
+            nomeDoCliente = sc.nextLine();
+            System.out.println("Saldo");
+            saldo = sc.nextBigDecimal();
+            System.out.println("---------------------------");
 
-                salvoComSucesso();
+            salvoComSucesso();
 
-                System.out.println("-------------------------");
-                System.out.println("1-Adicionar Conta");
-                System.out.println("0-Encerrar");
-                System.out.println("-------------------------");
-                opcao = sc.nextInt();
-                System.out.println("--------------------------");
-            }
+            System.out.println("-------------------------");
+            System.out.println("1-Adicionar Conta");
+            System.out.println("0-Encerrar");
+            System.out.println("-------------------------");
+            opcao = sc.nextInt();
+            System.out.println("--------------------------");
+        }
     }
-    public void salvoComSucesso(){
+
+    public void salvoComSucesso() {
         System.out.println(
                 "Olá " + nomeDoCliente + ", obrigado por criar uma conta em nosso banco," +
                         " sua agência é " + agencia + ", conta " + numero + " e seu saldo "
