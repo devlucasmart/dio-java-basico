5b7f16959612409d2044438e1f39d3b5b795a4a7
Lucas Martins <lucas.martins.fsa@gmail.com>
2023-08-15 15:08:38 -0300

    estudo sobre a classe Stack para estruturas de dados do tipo pilha

---
+++ EDD-exemplos/src/main/java/Main.java
@@ -0,0 +1,23 @@
+import dio.innovation.Carro;
+
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args){
+
+        Stack<Carro> stackCarros = new Stack<>();
+
+        stackCarros.push(new Carro("Ford"));
+        stackCarros.push(new Carro("Hyundai"));
+        stackCarros.push(new Carro("Honda"));
+        stackCarros.push(new Carro("Wolks"));
+
+        System.out.println(stackCarros);
+        System.out.println(stackCarros.pop());
+        System.out.println(stackCarros);
+
+        System.out.println(stackCarros.peek());
+        System.out.println(stackCarros);
+
+    }
+}
