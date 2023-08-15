a13cd69453aee896265e640700afa0a21eee3335
Lucas Martins <lucas.martins.fsa@gmail.com>
2023-08-15 11:09:19 -0300

    aula de estrutura de dados - busca arvore binaria

---
+++ Estrutura-De-Dados-JAVA/arvores/src/main/java/Main.java
@@ -0,0 +1,21 @@
+import dio.innovation.ArvoreBinaria;
+import dio.innovation.model.Obj;
+
+public class Main {
+    public static void main(String[] args){
+        ArvoreBinaria<Obj> minhaArvore = new ArvoreBinaria<>();
+
+        minhaArvore.inserir(new Obj(13));
+        minhaArvore.inserir(new Obj(10));
+        minhaArvore.inserir(new Obj(25));
+        minhaArvore.inserir(new Obj(2));
+        minhaArvore.inserir(new Obj(12));
+        minhaArvore.inserir(new Obj(20));
+        minhaArvore.inserir(new Obj(31));
+        minhaArvore.inserir(new Obj(29));
+
+        minhaArvore.exibirInOrder();
+        minhaArvore.exibirPreOrdem();
+        minhaArvore.exibirPosOrdem();
+    }
+}
