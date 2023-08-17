b5d3ddc4da0b7a098e0faa14685d83c368f5fd35
Lucas Martins <lucas.martins.fsa@gmail.com>
2023-08-17 09:29:28 -0300

    melhorias no exercicio 1

--- Collections/src/main/java/list/Execicio/Ex1/Execicio1.java
+++ Collections/src/main/java/list/Execicio/Ex1/Execicio1.java
@@ -1,39 +1,76 @@
 package list.Execicio.Ex1;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 
 public class Execicio1 {
     public static void main(String[] args) {
 
-        List<Double> temp = new ArrayList<>();
-        temp.add(25.0);
-        temp.add(30.0);
-        temp.add(40.0);
-        temp.add(28.8);
-        temp.add(20.5);
-        temp.add(21.4);
+        Scanner scan = new Scanner(System.in);
+        List<Double> temperaturas = new ArrayList<>();
 
-        List<String> meses = new ArrayList<>();
-        meses.add("Janeiro");
-        meses.add("Fevereiro");
-        meses.add("Março");
-        meses.add("Abril");
-        meses.add("Maio");
-        meses.add("Junho");
+        //Adicionando as temperaturas na lista:
+        int count = 0;
+        while (true) {
+            if (count == 6) break;
 
-        System.out.println("Temperaturas:");
-
-        for (int i = 0; i < temp.size(); i++) {
-            System.out.println(meses.get(i) + ": " + temp.get(i) + " Celsius");
+            System.out.print("Digite a temperatura: ");
+            double temp = scan.nextDouble();
+            temperaturas.add(temp);
+            count++;
         }
+        System.out.println("-----------------");
+        //exibindo todas as temperaturas:
+        System.out.print("Todas as temperaturas: ");
+        temperaturas.forEach(t -> System.out.print(t + " "));
 
-        Double soma = 0d;
-        for(Double value : temp){
-            soma += value;
+        //calculando e exibindo a média das temperaturas:
+        double media = temperaturas.stream()
+                .mapToDouble(Double::doubleValue)
+                .average()
+                .orElse(0d);
+        System.out.printf("\nMédia das temperaturas: %.1f\n", media);
+
+        //exibindo as temperaturas acima da média
+        System.out.print("Temperaturas acima da média: ");
+        temperaturas.stream()
+                .filter(t -> t > media)
+                .forEach(t -> System.out.printf("%.1f ", t));
+
+        //exibindo o mês das temperaturas acima da média por extenso:
+        System.out.println("\n\nMeses das temperaturas acima da média: ");
+        Iterator<Double> iterator = temperaturas.iterator();
+
+        count = 0;
+        while (iterator.hasNext()) {
+            Double temp = iterator.next();
+            if (temp > media) {
+                switch (count) {
+                    case (0):
+                        System.out.printf("1 - janeiro: %.1f\n ", temp);
+                        break;
+                    case (1):
+                        System.out.printf("2 - fevereiro: %.1f\n", temp);
+                        break;
+                    case (2):
+                        System.out.printf("3 - março: %.1f\n", temp);
+                        break;
+                    case (3):
+                        System.out.printf("4 - abril: %.1f\n", temp);
+                        break;
+                    case (4):
+                        System.out.printf("5 - maio: %.1f\n", temp);
+                        break;
+                    case (5):
+                        System.out.printf("6 - junho: %.1f\n", temp);
+                        break;
+                    default:
+                        System.out.println("Não houve temperatura acima da média.");
+                }
+            }
+            count++;
         }
-
-        double media = soma / temp.size();
-        System.out.println("Média de Temperatura: " + media);
     }
 }
