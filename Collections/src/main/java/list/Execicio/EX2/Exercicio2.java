675b59f61958b8532100a54fda1a175b71b7002e
Lucas Martins <lucas.martins.fsa@gmail.com>
2023-08-17 09:17:47 -0300

    melhorias no execicio 2

--- Collections/src/main/java/list/Execicio/EX2/Exercicio2.java
+++ Collections/src/main/java/list/Execicio/EX2/Exercicio2.java
@@ -1,52 +1,54 @@
 package list.Execicio.EX2;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Scanner;
 
 public class Exercicio2 {
     public static void main(String[] args){
 
-        List<String> perguntas = new ArrayList<>();
-        perguntas.add("Telefonou para a vítima?");
-        perguntas.add("Esteve no local do crime?");
-        perguntas.add("Mora perto da vítima?");
-        perguntas.add("Devia para a vítima?");
-        perguntas.add("Já trabalhou com a vítima?");
+        List<String> respostas = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
 
-        List<Boolean> resposta = new ArrayList<>();
-        resposta.add(false);
-        resposta.add(false);
-        resposta.add(false);
-        resposta.add(false);
-        resposta.add(false);
-        resposta.add(false);
+        System.out.println("Perguntas: ");
+        System.out.print("\nTelefonou para vítima? ");
+        String resposta = scanner.next();
+        respostas.add(resposta.toLowerCase());
+        System.out.print("Esteve no local do crime? ");
+        resposta = scanner.next();
+        respostas.add(resposta.toLowerCase());
+        System.out.print("Mora perto da vítima? ");
+        resposta = scanner.next();
+        respostas.add(resposta.toLowerCase());
+        System.out.print("Devia para vítima? ");
+        resposta = scanner.next();
+        respostas.add(resposta.toLowerCase());
+        System.out.print("Já trabalhou com a vítima? ");
+        resposta = scanner.next();
+        respostas.add(resposta.toLowerCase());
 
-        int verdadeiros = 0;
-        int falsos = 0;
+        System.out.println(respostas);
 
-        for (Boolean resp : resposta) {
-            if (resp) {
-                verdadeiros++;
-            } else {
-                falsos++;
+        int count = 0;
+        Iterator<String> contador = respostas.iterator();
+        while(contador.hasNext()){
+            String resp = contador.next();
+            if(resp.contains("s")) {
+                count ++;
             }
         }
 
-        System.out.println("Perguntas e Respostas:");
-        for (int i = 0; i < perguntas.size(); i++) {
-            System.out.println(perguntas.get(i) + ": " + resposta.get(i));
-        }
-
-        System.out.println("\nClassificação:");
-
-        if (verdadeiros == 3 || verdadeiros == 4) {
-            System.out.println("Cúmplice");
-        } else if (verdadeiros == 2) {
-            System.out.println("Suspeito");
-        } else if (verdadeiros == 5) {
-            System.out.println("Assassina");
-        } else {
-            System.out.println("Inocente");
+        switch(count) {
+            case 2:
+                System.out.println(">> SUSPEITA <<"); break;
+            case 3:
+            case 4:
+                System.out.println(">> CÚMPLICE <<"); break;
+            case 5:
+                System.out.println(">> ASSASSINO <<"); break;
+            default:
+                System.out.println(">> INOCENTE <<"); break;
         }
     }
 }
